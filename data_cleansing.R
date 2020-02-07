59a6d91026373ccd47feb87dc6f4a734b42fab46
allenjli <allenjli@umich.edu>
2020-02-07 16:13:32 -0500

    first upload :joy:

---
+++ data_cleansing.R
@@ -0,0 +1,13 @@
+#hi
+#Aim1: To determine disparities in polytobacco use by SES and 
+#race/ethnicity and to monitor changes over time in consumption patterns; 
+
+#Aim 2: To estimate the impact of tobacco control policies on 
+#patterns of tobacco and nicotine product use by SES and race/ethnicity;
+
+#Aim 3: To determine downstream tobacco-related health
+#disparities in all-cause mortality, cardiovascular mortality, 
+#and other health outcomes associated with individual- and poly-tobacco use;
+
+#Aim 4: To model the impact of potential policies on tobacco-related
+#health disparities associated with individual- and poly-tobacco use;
